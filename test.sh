1c2e89c88fdef50d213f733334ee1d3b8b189d58
Dominik Falkner <dominik.falkner@risc-software.at>
2023-07-04 12:48:47 +0000

    fix: Add structure

---
+++ test.sh
@@ -0,0 +1,4 @@
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
