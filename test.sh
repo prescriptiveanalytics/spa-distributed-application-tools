7b2217480f815d01751e73aa4c1f3406c151ed8f
Dominik Falkner <dominik.falkner@risc-software.at>
2023-09-13 14:24:19 +0200

    chore: Delete unused files

--- test.sh
+++
@@ -1,4 +0,0 @@
-echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-  tee /etc/apt/sources.list.d/docker.list > /dev/null
